5f5584d9fca0b250920f4cb953d279f3a62fb5f0
Shahzadsaroya <scruz252500@gmail.com>
2021-08-05 15:47:35 +0500

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final int days = 30;
+    final String name = "Shahzad";
+    var wf = 3.45;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My App"),
+      ),
+      body: Center(
+        child: Material(
+          child: Center(
+            child: Container(
+              child: Text("$name using $wf & started $days flutter course"),
+            ),
+          ),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
