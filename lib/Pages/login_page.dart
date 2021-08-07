6ac23321b1ee6053b830dfe415c08ecfe2b5c1e7
Shahzadsaroya <scruz252500@gmail.com>
2021-08-07 18:27:35 +0500

    day4 completed

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -7,16 +7,57 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Material(
-          child: Center(
-        child: Text(
-          "Welcome Login Page",
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.teal.shade400,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      )),
+          color: Colors.white,
+          child: Column(children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter User Name",
+                      labelText: "User Name",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(),
+                    onPressed: () {
+                      print("Hi Shahzad");
+                    },
+                  ),
+                ],
+              ),
+            )
+          ])),
     );
   }
 }
